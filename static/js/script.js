68faff7e7891d0299c4ddafd2fbd15da9a6b212e
Sivasubramanyam A <sivasubramanyama@gmail.com>
2016-04-05 00:17:12 +0530

    Initial COmmit

---
+++ static/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    var temp_image, first_save;
+
+    var range_slider = $("#area").slider({
+        min: 1,
+        max: 100,
+    });
+
+    $('#image-submit').click(function (argument) {
+
+        var formData = new FormData($('#image-upload')[0]);
+        $.ajax({
+
+            url : '/upload',
+            dataType: 'json',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                console.log(response);
+                if (response['error'] == 0) {
+                    temp_image = response['url'];
+                    $('#result').attr('src', temp_image);
+                    formData.append('url', temp_image);
+                };
+            }
+
+        });
+
+    });
+
+});
